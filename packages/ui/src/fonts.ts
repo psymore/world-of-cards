333333747b4c8f58c721e669623d6890f1a753d8
Ege Özel <egeeozel@gmail.com>
2026-08-14 13:51:28 +0300

    feat(ui): add Cinzel + Inter font assets

--- packages/ui/src/fonts.ts
+++ packages/ui/src/fonts.ts
@@ -1,6 +1,10 @@
 export const FONTS = {
   'PTSerif-Bold': require('../assets/fonts/PTSerif-Bold.ttf'),
   'PTSerif-Regular': require('../assets/fonts/PTSerif-Regular.ttf'),
+  'Cinzel-Regular': require('../assets/fonts/Cinzel-Regular.ttf'),
+  'Cinzel-Bold': require('../assets/fonts/Cinzel-Bold.ttf'),
+  'Inter-Regular': require('../assets/fonts/Inter-Regular.ttf'),
+  'Inter-Bold': require('../assets/fonts/Inter-Bold.ttf'),
 };
 
 // Change this to 'PTSerif-Regular' to switch the card corner-index weight.
